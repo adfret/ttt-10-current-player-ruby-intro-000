8d4cf0c3fb53fd6f24071af6808f5ea9aed3c9fb
Ad Friedman <adien330@yahoo.com>
2018-12-18 18:45:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,25 @@
 def turn_count(board)
 count = 0
-if board.count{|i| i =="X"} == 1
-  count+=1
-end
-while count >= 1
-  while board.count{|i| i == "O"} +=1
-    count += 2
-  end
-end
 
 
+if board.count{|i| i == "X"} == 1 
+count+=1
+elsif board.count{|i| i == "X"} == 1 and board.count{|i| i == "O"} == 1
+count+=2
+elsif board.count{|i| i == "X"} == 2 and  board.count{|i| i == "O"} == 1
+count+=3
+elsif board.count{|i| i == "X"} == 2 and  board.count{|i| i == "O"} == 2
+count+=4
+elsif board.count{|i| i == "X"} == 3 and  board.count{|i| i == "O"} == 2
+count+=5
+elsif board.count{|i| i == "X"} == 3 and  board.count{|i| i == "O"} == 3
+count+=6
+elsif board.count{|i| i == "X"} == 4 and  board.count{|i| i == "O"} == 3
+count+=7
+elsif board.count{|i| i == "X"} == 4 and  board.count{|i| i == "O"} == 4
+count+=8
+elsif board.count{|i| i == "X"} == 5 and  board.count{|i| i == "O"} == 4
+count+=9
+end 
 
 end
