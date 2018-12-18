cc3f4d3afcadcf27fdb62a847a7deff75432c911
Ad Friedman <adien330@yahoo.com>
2018-12-18 18:59:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -25,11 +25,14 @@ end
 
 
 def current_player(board)
-if turn_count(board)%2 == 0
-return "O"
-elsif turn_count(board)%2 == 1
+  if turn_count(board) == 0 
+    return "X"
+  elsif turn_count(board) == 1 
+    return "O"
+elsif turn_count(board)%2 == 0
 return "X"
-else
-  return "X"
+elsif turn_count(board)%2 == 1
+return "O"
+end
 end
 end
